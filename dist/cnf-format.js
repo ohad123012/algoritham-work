497362e8322de2a51b41af4a357eb1d886d279ea
ohad123012 <ohadlazar12@gmail.com>
2024-10-31 11:28:49 +0200

    feat: finished CNF question

--- dist/cnf-format.js
+++ dist/cnf-format.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 let allStringCombinationsAppearance = [];
-const JsonFile = fs.readFileSync(path.resolve("assets/question1.json"), {
+const JsonFile = fs.readFileSync(path.resolve("assets/question5.json"), {
     encoding: "utf-8",
 });
 const jsonObject = JSON.parse(JsonFile);
@@ -19,17 +19,19 @@ const runThroguhNodes = (roots) => {
             return runThroguhNodes(roots.children);
         }
         else if (roots.maxStations > 1) {
-            AddCanisterToGlobalString(roots.name);
+            AddCanisterToGlobalString(roots.name, roots.weight);
             if (Array.isArray(roots.children)) {
                 for (const child of roots.children) {
-                    addToLocalString(child.name, canisterBombCheck);
+                    addToLocalString(child.name, canisterBombCheck, child.weight);
                 }
             }
         }
-        console.log(canisterBombCheck);
+        canisterBombCheck.forEach((specificBombInCanister) => {
+            addBombsToGlobalString(specificBombInCanister.stringCombination, specificBombInCanister.amountAppearance, roots.maxStations, specificBombInCanister.weight);
+        });
     }
 };
-const addToLocalString = (bombName, canisterBombToAdd) => {
+const addToLocalString = (bombName, canisterBombToAdd, weight) => {
     const doesHaveValue = canisterBombToAdd.some((bombWithCounter) => bombWithCounter.stringCombination === bombName);
     if (doesHaveValue) {
         canisterBombToAdd.forEach((bombWithCounter) => {
@@ -43,11 +45,38 @@ const addToLocalString = (bombName, canisterBombToAdd) => {
         canisterBombToAdd.push({
             stringCombination: bombName,
             amountAppearance: 1,
+            weight: weight,
         });
     }
 };
-const addBombsToGlobalString = (name, amountOfBomb, maxStations, weight) => { };
-const AddCanisterToGlobalString = (canisterName) => {
+const addBombsToGlobalString = (bombName, amountOfBomb, maxStations, weight) => {
+    let stringArray = [
+        amountOfBomb.toString(),
+        "/",
+        maxStations.toString(),
+        " ",
+        bombName,
+    ];
+    const bombCombinationString = stringArray.join("");
+    //   console.log(bombCombinationString);
+    const doesHaveValue = allStringCombinationsAppearance.some((valueInAppearance) => valueInAppearance.stringCombination === bombCombinationString);
+    if (doesHaveValue) {
+        allStringCombinationsAppearance.forEach((valueInAppearance) => {
+            if (valueInAppearance.stringCombination === bombCombinationString) {
+                valueInAppearance.amountAppearance =
+                    valueInAppearance.amountAppearance + 1;
+            }
+        });
+    }
+    else {
+        allStringCombinationsAppearance.push({
+            stringCombination: bombCombinationString,
+            amountAppearance: 1,
+            weight: weight,
+        });
+    }
+};
+const AddCanisterToGlobalString = (canisterName, weight) => {
     const doesHaveValue = allStringCombinationsAppearance.some((valueInAppearance) => valueInAppearance.stringCombination === canisterName);
     if (doesHaveValue) {
         allStringCombinationsAppearance.forEach((valueInAppearance) => {
@@ -61,8 +90,62 @@ const AddCanisterToGlobalString = (canisterName) => {
         allStringCombinationsAppearance.push({
             stringCombination: canisterName,
             amountAppearance: 1,
+            weight: weight,
         });
     }
 };
+const createCanistersStringForCnf = () => {
+    let stringBombsCombination = "";
+    allStringCombinationsAppearance.forEach((combination) => {
+        if (combination.amountAppearance === 1) {
+            let stringArray = [`(${combination.stringCombination})`, "+"];
+            stringBombsCombination += stringArray.join("");
+        }
+        else {
+            let stringArray = [
+                combination.amountAppearance.toString(),
+                "x",
+                `(${combination.stringCombination})`,
+                "+",
+            ];
+            stringBombsCombination += stringArray.join("");
+        }
+    });
+    stringBombsCombination = stringBombsCombination.slice(0, -1);
+    return stringBombsCombination;
+};
+const createMultiplyString = () => {
+    let totalWeight = 0;
+    allStringCombinationsAppearance.forEach((combination) => {
+        const numberOfBombs = combination.stringCombination.substring(0, 1);
+        if (!isNaN(Number(numberOfBombs))) {
+            totalWeight +=
+                Number(numberOfBombs) *
+                    combination.amountAppearance *
+                    combination.weight;
+        }
+        else {
+            totalWeight +=
+                combination.amountAppearance *
+                    combination.weight;
+        }
+    });
+    console.log(totalWeight);
+    return totalWeight;
+};
+const createCnf = (bombString, totalWeight) => {
+    const stringForCnf = `${bombString}\n = ${totalWeight}`;
+    fs.writeFile(path.resolve("assets/answer5.cnf"), stringForCnf, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("done");
+    });
+    console.log("created file");
+};
 parseArmamentChain(enteredTree);
+console.log(allStringCombinationsAppearance);
+const bombString = createCanistersStringForCnf();
+const totalWeight = createMultiplyString();
+createCnf(bombString, totalWeight);
 //# sourceMappingURL=cnf-format.js.map
--- src/cnf-format.ts
+++ src/cnf-format.ts
@@ -8,12 +8,12 @@ export interface Node {
   maxStations: number;
 }
 
-type fieldsForRecord = "stringCombination" | "amountAppearance";
+type fieldsForRecord = "stringCombination" | "amountAppearance" | "weight";
 type appearanceType = Record<fieldsForRecord, string | number>;
 
 let allStringCombinationsAppearance: appearanceType[] = [];
 
-const JsonFile = fs.readFileSync(path.resolve("assets/question1.json"), {
+const JsonFile = fs.readFileSync(path.resolve("assets/question5.json"), {
   encoding: "utf-8",
 });
 
@@ -35,19 +35,28 @@ const runThroguhNodes = (roots: Node[] | Node) => {
     if (roots.maxStations == 1) {
       return runThroguhNodes(roots.children);
     } else if (roots.maxStations > 1) {
-      AddCanisterToGlobalString(roots.name);
+      AddCanisterToGlobalString(roots.name, roots.weight);
       if (Array.isArray(roots.children)) {
         for (const child of roots.children) {
-          addToLocalString(child.name, canisterBombCheck);
+          addToLocalString(child.name, canisterBombCheck, child.weight);
         }
       }
     }
-    console.log(canisterBombCheck);
+
+    canisterBombCheck.forEach((specificBombInCanister) => {
+      addBombsToGlobalString(
+        specificBombInCanister.stringCombination as string,
+        specificBombInCanister.amountAppearance as number,
+        roots.maxStations,
+        specificBombInCanister.weight as number
+      );
+    });
   }
 };
 const addToLocalString = (
   bombName: string,
-  canisterBombToAdd: appearanceType[]
+  canisterBombToAdd: appearanceType[],
+  weight: number
 ) => {
   const doesHaveValue = canisterBombToAdd.some(
     (bombWithCounter) => bombWithCounter.stringCombination === bombName
@@ -64,6 +73,7 @@ const addToLocalString = (
     canisterBombToAdd.push({
       stringCombination: bombName,
       amountAppearance: 1,
+      weight: weight,
     });
   }
 };
@@ -74,14 +84,35 @@ const addBombsToGlobalString = (
   maxStations: number,
   weight: number
 ) => {
-  // const bombCombinationString = amountOfBomb/maxStations bombName
+  let stringArray = [
+    amountOfBomb.toString(),
+    "/",
+    maxStations.toString(),
+    " ",
+    bombName,
+  ];
+  const bombCombinationString = stringArray.join("");
   const doesHaveValue = allStringCombinationsAppearance.some(
     (valueInAppearance) =>
       valueInAppearance.stringCombination === bombCombinationString
   );
+  if (doesHaveValue) {
+    allStringCombinationsAppearance.forEach((valueInAppearance) => {
+      if (valueInAppearance.stringCombination === bombCombinationString) {
+        valueInAppearance.amountAppearance =
+          (valueInAppearance.amountAppearance as number) + 1;
+      }
+    });
+  } else {
+    allStringCombinationsAppearance.push({
+      stringCombination: bombCombinationString,
+      amountAppearance: 1,
+      weight: weight,
+    });
+  }
 };
 
-const AddCanisterToGlobalString = (canisterName: string) => {
+const AddCanisterToGlobalString = (canisterName: string, weight: number) => {
   const doesHaveValue = allStringCombinationsAppearance.some(
     (valueInAppearance) => valueInAppearance.stringCombination === canisterName
   );
@@ -97,7 +128,65 @@ const AddCanisterToGlobalString = (canisterName: string) => {
     allStringCombinationsAppearance.push({
       stringCombination: canisterName,
       amountAppearance: 1,
+      weight: weight,
     });
   }
 };
+
+const createCanistersStringForCnf = () => {
+  let stringBombsCombination = "";
+  allStringCombinationsAppearance.forEach((combination) => {
+    if (combination.amountAppearance === 1) {
+      let stringArray = [`(${combination.stringCombination as string})`, "+"];
+      stringBombsCombination += stringArray.join("");
+    } else {
+      let stringArray = [
+        combination.amountAppearance.toString(),
+        "x",
+        `(${combination.stringCombination as string})`,
+        "+",
+      ];
+      stringBombsCombination += stringArray.join("");
+    }
+  });
+  stringBombsCombination = stringBombsCombination.slice(0, -1);
+
+  return stringBombsCombination;
+};
+
+const createMultiplyString = () => {
+  let totalWeight = 0;
+  allStringCombinationsAppearance.forEach((combination) => {
+    const numberOfBombs = (combination.stringCombination as string).substring(
+      0,
+      1
+    );
+
+    if (!isNaN(Number(numberOfBombs))) {
+      totalWeight +=
+        Number(numberOfBombs) *
+        (combination.amountAppearance as number) *
+        (combination.weight as number);
+    } else {
+      totalWeight +=
+        (combination.amountAppearance as number) *
+        (combination.weight as number);
+    }
+  });
+  return totalWeight;
+};
+const createCnf = (bombString: string, totalWeight: number) => {
+  const stringForCnf = `${bombString}\n = ${totalWeight}`;
+
+  fs.writeFile(path.resolve("assets/answer5.cnf"), stringForCnf, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("done");
+  });
+};
+
 parseArmamentChain(enteredTree as Node[]);
+const bombString = createCanistersStringForCnf();
+const totalWeight = createMultiplyString();
+createCnf(bombString, totalWeight);
